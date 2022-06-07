de4f94d3224b57e0c861a75dcbe156083358e8c8
Gleison <gleison04lemos@gmail.com>
2022-06-06 21:58:05 -0300

    Validação de CNJP e refatoração de código

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-
-from cpf import Cpf
-
-cpf = Cpf("07269049378")
-
-print(cpf)
+from cpf_cnpj import Documento
+from validate_docbr import CNPJ
+#
+exemplo_cnpj = "353798380001123"
+#exemplo_cpf = "96002196056"
+# #cnpj = CNPJ()
+# #print(cnpj.validate_exemplo_cnpj))
+#
+documento = Documento.cria_documento(exemplo_cnpj)
+print(documento)
