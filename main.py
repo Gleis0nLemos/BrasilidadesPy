5be153f7af1245a2cbb126e7e25b53ea821183a3
Gleison <gleison04lemos@gmail.com>
2022-06-07 21:58:50 -0300

    Usando datatime e formatações afins

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-from brphones import Telefone
-import re
+from datetime import datetime, timedelta
+from brdates import Datas
 
-telefone = "5585992970088"
-i = Telefone(telefone)
-print(i)
-
+hoje = Datas()
+print(hoje.tempo_cadastro())
 
 
 
-#>>>>LISTA DE CARACTERES IMPORTANRTES PARA SE TRABALHAR COM REGEX
+
+
+
+
+
+#>>>>>>>>>>>>>LISTA DE CARACTERES IMPORTANRTES PARA SE TRABALHAR COM REGEX<<<<<<<<<<<<<<<<<
 # []	Define um range ou um grupo de caracteres	                    [0-9]; [a-z]; [abc]
 #  *	Marca nenhuma, uma ou mais ocorrências	                        sol*
 # {}	Quantidade de repetições de uma ocorrência definida	            [abc]{5}
 # \d	Qualquer número de 0 até 9	                                    \d{3,4}
-# \w	Qualquer número de a té 9 letra de a até z ou _	                \w{10}
+# \w	Qualquer número de 0 até 9 ou letra de a até z ou _	            \w{10}
 # |	    Um ou outro                                                     @$
 # ()	Captura e agrupa	                                            (\w{4})
+# %A	Dias da semana por extenso	                                    Sunday, Monday, ...
+# %d	Dias do mês	                                                    01, 02, ..., 31
+# %m	Meses em formato de números	                                    01, 02, ..., 12
+# %y	Ano em formato de 2 dígitos	                                    99, 15
+# %Y	Ano em formato de 4 dígitos	                                    1993, 2011
+# %H	Hora em formato decimal	                                        00, 01, ..., 23
+# %M	Minuto em formato decimal	                                    00, 01, ..., 59
+# %S	Segundo em formato decimal	                                    00, 01, ..., 59
